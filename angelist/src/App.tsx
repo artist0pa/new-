b37a55a1234c4dc23ad132a74ea716d4eef8916d
md azam <130434238+iiamAzam@users.noreply.github.com>
2024-10-04 06:06:24 +0000

    angelist initialization

---
+++ angelist/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <div className=' bg-red-500 w-[100px] h-[100px]'>
+      <p className=' text-amber-900'>ok this gon a work in </p>
+     </div>
+    </>
+  )
+}
+
+export default App
